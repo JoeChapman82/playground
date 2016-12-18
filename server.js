0d958ce7a1dde122bb16a772048396256568feda
Joe Chapman <contemno82@gmail.com>
2016-12-18 10:25:49 +0000

    made it work

--- server.js
+++ server.js
@@ -2,9 +2,6 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-  res.send('Todo API Root');
-});
 
 app.use(express.static(__dirname + '/playground'));
 
