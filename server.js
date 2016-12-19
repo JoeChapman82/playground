bf62b37058188b42a030d6acaa94b2e84ea9451d
Joe Chapman <contemno82@gmail.com>
2016-12-19 19:59:19 +0000

    d

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 
-app.use(express.static('playground'));
+app.use(express.static(__dirname + '/playground'));
 
 app.listen(PORT, function () {
   console.log('Express listening on port ' + PORT + '!');
